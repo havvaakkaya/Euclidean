fdb10590730ea3b135ecdeefe0e3d9e246b07eb1
havvaakkaya <havvanur25000@gmail.com>
2024-12-25 07:37:01 +0300

    Euclidean.py

--- Euclidean.py
+++ Euclidean.py
@@ -1,12 +1,10 @@
 
-points = [(1, 2), (3, 4), (6, 8), (2, 1)]
-
-
 def euclideanDistance(point1, point2):
     x1, y1 = point1
     x2, y2 = point2
     return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
-
+    
+points = [(1, 2), (3, 4), (6, 8), (2, 1)]
 
 distances = []
 for i in range(len(points)):
